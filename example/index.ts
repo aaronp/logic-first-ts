5d4279809162f9eb5de5ad53a40afa19f950f45c
aaron.pritzlaff <aaron@kindservices.co.uk>
2025-02-10 14:06:01 +0000

    updated example

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,5 @@
-import { traceSpan, newPerson, newSystem } from "logic-first";
+import { Tracer, newPerson, newSystem } from "logic-first";
+import fs from 'fs'
 
 const person = newPerson('app', 'dave')
 const sys = newSystem('app', 'parser')
@@ -6,7 +7,7 @@ const goog = newSystem('app', 'google')
 
 // Function to perform a Google search
 async function googleSearch(query: string) {
-  const span = await traceSpan(sys, goog, 'googleSearch', [query], async () => {
+  const span = await Tracer.instance().trace(sys, goog, 'googleSearch', [query], async () => {
     const response = await fetch(`https://www.google.com/search?q=${encodeURIComponent(query)}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -20,7 +21,7 @@ async function googleSearch(query: string) {
 
 // Function to parse user input and split it into words
 async function parseInput(input: string) {
-  return await traceSpan(person, sys, 'parse', [input], async () => await googleSearch(input));
+  return await Tracer.instance().trace(person, sys, 'parse', [input], async () => await googleSearch(input));
 }
 
 // Main function
@@ -36,6 +37,16 @@ async function main() {
     const words = await parseInput(input);
 
     console.log('got: ' + JSON.stringify(words, null, 2))
+
+    const logicFirst = Tracer.instance().logicFirst()
+    const mermaidDiagram = logicFirst.mermaid.markdown();
+    fs.writeFileSync('mermaid.md', mermaidDiagram);
+  
+    const plant = logicFirst.plantUML.diagram("App");
+    fs.writeFileSync('plant.puml', plant);
+  
+    const c4 = logicFirst.c4.diagram();
+    fs.writeFileSync('diagram.c4', c4);
 }
 
 // Run the main function
